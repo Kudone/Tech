3bdfc9301e0a6effbe11adcb381398a7b53f54e3
kudone <ku.kn8821@gmail.com>
2019-02-09 06:53:39 +0900

    no message

---
+++ Assets/_Scripts/A_Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public GameObject effectObj;
+
+
+	// Use this for initialization
+	void Start () {
+		//弾を5秒後に削除する
+		Destroy (this.gameObject, 2);
+	}
+
+	void Update(){
+		
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "Stage") {
+			GameObject effect = Instantiate (effectObj, gameObject.transform.position, effectObj.transform.rotation);
+			effect.transform.parent = gameObject.transform;
+
+			print ("aa");
+		}
+	}
+}
---
+++ Assets/_Scripts/A_Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	// speedを制御する
+	float speed = 50f;
+
+
+	void Update ()
+	{
+		float x =  Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+		// xとyにspeedを掛ける
+		rigidbody.AddForce(x * speed, 0, z * speed);
+
+	}
+
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.C)) {
+			this.transform.localScale = new Vector3 (1f, 0.5f, 1f);
+			speed = 20f;
+		} 
+
+		else if(Input.GetKeyUp(KeyCode.C)) {
+			this.transform.localScale = new Vector3 (1f, 1f, 1f);
+			speed = 50f;
+		}
+
+	}  
+}
